c260ed694965799296999b8a5fffaa7c0341d0b3
billy <nduclong6201@gmail.com>
2025-04-08 07:06:25 +0700

    Configures Next.js and PostCSS
    Configures Next.js to use a custom dist directory and disables the `poweredByHeader`.
    
    Updates PostCSS configuration to use the `@tailwindcss/postcss` plugin.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  distDir: ".next",
+  poweredByHeader: false,
 };
 
 export default nextConfig;
