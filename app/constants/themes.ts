9e9f29e997378af324a5f570d2d53b78e2ffb6a6
billy <nduclong6201@gmail.com>
2025-04-09 09:39:27 +0700

    Updates predefined color values
    Updates the hex values for the predefined color options in the color picker.
    These adjusted color values enhance the visual appearance and improve color consistency.

--- app/constants/themes.ts
+++ app/constants/themes.ts
@@ -1,13 +1,13 @@
 // Predefined color options for the color picker
 export const PREDEFINED_COLORS = {
-  red: "#D95550",
-  teal: "#3D948B",
-  blue: "#4C83B6",
-  gold: "#B69D4C",
-  purple: "#9C5EB1",
-  pink: "#C96D9C",
-  green: "#4E9D6D",
-  gray: "#646978",
+  red: "#BA4949",
+  teal: "#388588",
+  blue: "#397097",
+  gold: "#9B8238",
+  purple: "#7D53A2",
+  pink: "#AF4E91",
+  green: "#518A58",
+  gray: "#545764",
 };
 
 // Default theme colors
